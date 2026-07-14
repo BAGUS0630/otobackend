0511ab93c190b1f150bb71d77ea28d5b7e1d2b43
Rafli <raflimramadhan.204@gmail.com>
2026-07-14 07:54:02 +0700

    update

--- handler/stats_handler.go
+++ handler/stats_handler.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"github.com/gofiber/fiber/v2"
 	"otomeet-backend/config"
+	"otomeet-backend/model"
 )
 
 // GetStats mengembalikan statistik singkat untuk dashboard frontend
@@ -11,13 +12,13 @@ func GetStats(c *fiber.Ctx) error {
 	var touringsCount int64
 	var registrationsCount int64
 
-	config.DB.Model(&map[string]interface{}{}).Table("users").Count(&usersCount)
-	config.DB.Model(&map[string]interface{}{}).Table("tourings").Count(&touringsCount)
-	config.DB.Model(&map[string]interface{}{}).Table("registrations").Count(&registrationsCount)
+	config.DB.Model(&model.User{}).Count(&usersCount)
+	config.DB.Model(&model.Touring{}).Count(&touringsCount)
+	config.DB.Model(&model.Registration{}).Count(&registrationsCount)
 
 	return c.JSON(fiber.Map{
-		"users":         usersCount,
-		"tourings":      touringsCount,
-		"registrations": registrationsCount,
+		"total_users":         usersCount,
+		"total_tourings":      touringsCount,
+		"total_registrations": registrationsCount,
 	})
 }
